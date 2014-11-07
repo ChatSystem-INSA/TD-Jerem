8f1f13eb5efec56a849eec0693eabc389baad0df
tlk <jeremieboutoille@gmail.com>
2014-11-07 11:30:34 +0100

    Ajout d'une superbe interface de ouf

---
+++ ButtonListener.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Banana on 07/11/14.
+ */
+public class ButtonListener implements ActionListener {
+
+    private Fenetre fenetre;
+
+    public ButtonListener(Fenetre fenetre) {
+        this.fenetre = fenetre;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        this.fenetre.afficherNom();
+    }
+}
--- Fenetre.java
+++ Fenetre.java
@@ -9,6 +9,7 @@ public class Fenetre extends JFrame {
     private JLabel label;
     private JTextField text;
     private JButton button;
+    private JLabel disp_text;
 
     public Fenetre(String title) {
         super();
@@ -20,13 +21,25 @@ public class Fenetre extends JFrame {
         label = new JLabel("Nom :");
         text = new JTextField(20);
         button = new JButton("OK");
+        disp_text = new JLabel();
+
+        button.addActionListener(new ButtonListener(this));
 
         this.setLayout(new BorderLayout());
         this.add("West", label);
         this.add("Center", text);
         this.add("East", button);
+        this.add("South", disp_text);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.pack();
         this.setVisible(true);
+    }
+
+    void afficherNom()
+    {
+        this.disp_text.setText(this.text.getText());
+        this.text.setText("");
+        this.pack();
     }
 
     public static void main(String[] args)

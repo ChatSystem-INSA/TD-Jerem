4203ee7a1ee155b16f7d038b58766d36dbd5804b
tlk <jeremieboutoille@gmail.com>
2014-11-07 08:42:06 +0100

    Add interface & class

---
+++ Publication.java
@@ -0,0 +1,26 @@
+
+public class Publication {
+    private String titre;
+    private int annee;
+
+    public Publication(String titre, int annee) {
+        this.titre = titre;
+        this.annee = annee;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public int getAnnee() {
+        return annee;
+    }
+
+    public void setAnnee(int annee) {
+        this.annee = annee;
+    }
+}
